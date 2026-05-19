2a87a5e1e177451ce5d154bebcdafe11c01dc89d
Souradip Pal <souradip1000@gmail.com>
2026-05-19 19:55:28 +0530

    Worker implementation added

--- src/main.rs
+++ src/main.rs
@@ -1,2 +1,37 @@
 type Job = Box<dyn FnOnce() + Send + 'static>;
 
+use std::sync::{Arc, Mutex};
+use std::sync::mpsc;
+use std::thread;
+
+struct Worker {
+    id: usize,
+    thread: Option<thread::JoinHandle<()>>,
+}
+
+impl Worker {
+    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
+        let thread = thread::spawn(move || {
+            loop {
+                let message = receiver
+                    .lock()       // acquire the mutex
+                    .unwrap()     // panic if the mutex is poisoned
+                    .recv();       // wait for a job (blocks here)
+
+                match message {
+                    Ok(job) => {
+                        job();       // run the job
+                    }
+                    Err(_) => {
+                        break;       // channel closed → shut down
+                    }
+                }
+            }
+        });
+
+        Worker {
+            id,
+            thread: Some(thread),
+        }
+    }
+}
