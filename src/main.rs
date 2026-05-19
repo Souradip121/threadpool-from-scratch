ec8d1f1ee7a739f3ba9e6f8a32ba8713df6909a7
Souradip Pal <souradip1000@gmail.com>
2026-05-19 20:37:07 +0530

    execute added in main.rs

--- src/main.rs
+++ src/main.rs
@@ -64,4 +64,17 @@ impl ThreadPool {
             sender: Some(sender),
         }
     }
+    pub fn execute<F>(&self, f: F)
+    where
+        F: FnOnce() + Send + 'static,
+    {
+        let job = Box::new(f);
+
+        self.sender
+            .as_ref()
+            .unwrap()
+            .send(job)
+            .unwrap();
+    }
 }
+
